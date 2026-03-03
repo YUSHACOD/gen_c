e71606c631e73c80e5bb050190f3dad66d5f2513
Ayush Paranjale <ayushparanjale@gmail.com>
2026-03-03 10:57:38 +0530

    simple generators for hooks and hooks table

--- gnrtr/gnrtr.go
+++ gnrtr/gnrtr.go
@@ -11,11 +11,10 @@ type StructField struct {
 	Type string
 }
 
-const struct_field_template = "    {{.Type}} {{.Name}};\n"
-
 func GenStruct(name string, fields []StructField) (string, error) {
-	field_t := template.Must(template.New("field_t").Parse(struct_field_template))
 
+	const struct_field_template = "    {{.Type}} {{.Name}};\n"
+	field_t := template.Must(template.New("field_t").Parse(struct_field_template))
 
 	var res strings.Builder
 
@@ -24,10 +23,82 @@ func GenStruct(name string, fields []StructField) (string, error) {
 	for _, field := range fields {
 		err := field_t.Execute(&res, field)
 		if err != nil {
-			return "", fmt.Errorf("Error executing field %v template: %v", field, err)
+			return "", fmt.Errorf("Error executing field %v : %v", field, err)
 		}
 	}
 
 	fmt.Fprintf(&res, "} %s;\n", name)
+	return res.String(), nil
+}
+
+type FuncType struct {
+	Name   string
+	Params string
+	Args   string
+	Return string
+}
+
+func GenFuncType(funcType FuncType) (string, error) {
+
+	const type_template = "typedef {{.Return}} {{.Name}}_F_TYPE{{.Args}};\n"
+	type_template_t := template.Must(template.New("type_template").Parse(type_template))
+
+	var res strings.Builder
+	err := type_template_t.Execute(&res, funcType)
+	if err != nil {
+		return "", fmt.Errorf("Error executing func type %v : %v", funcType, err)
+	}
+
+	return res.String(), nil
+}
+
+func GenHook(funcType FuncType) (string, error) {
+	const hook_template = `
+static {{.Return}} (WINAPI *og_{{.Name}}){{.Params}} = {{.Name}};
+static {{.Return}} WINAPI hooked_{{.Name}}{{.Params}} {
+
+    if(IsDebuggerPresent()) {
+    	__debugbreak();
+    }
+	{{if eq .Return "void"}}
+	TIME({ og_{{.Name}}{{.Args}}; });
+	{{else}}
+    {{.Return}} result;
+    TIME({ result = og_{{.Name}}{{.Args}}; });
+
+    if(IsDebuggerPresent()) {
+    	__debugbreak();
+    }
+
+    return result;
+	{{end}}
+}
+`
+	hook_template_t := template.Must(template.New("hook_template").Parse(hook_template))
+
+	var res strings.Builder
+	err := hook_template_t.Execute(&res, funcType)
+	if err != nil {
+		return "", fmt.Errorf("Error executing func type %v : %v", funcType, err)
+	}
+
+	return res.String(), nil
+}
+
+func GenHookTable(funcs []FuncType) (string, error) {
+	const table_element_template = "{&(void *&)og_{{.Name}}, (void *)hooked_{{.Name}}},\n"
+	table_element_template_t := template.Must(template.New("element_template").Parse(table_element_template))
+
+	var res strings.Builder
+	res.WriteString("static Hook GLBL_hooks[] = {\n")
+
+	for _, f := range funcs {
+		err := table_element_template_t.Execute(&res, f)
+		if err != nil {
+			return "", fmt.Errorf("Error executing func element %v : %v", f, err)
+		}
+	}
+
+	res.WriteString("};\n")
 	return res.String(), nil
 }
--- main.go
+++ main.go
@@ -9,20 +9,59 @@ import (
 func main() {
 	fmt.Printf("Generating c code\n")
 
-	fields := make([]gnrtr.StructField, 3)
-	fields[0].Name = "X"
-	fields[0].Type = "int"
+	// fields := make([]gnrtr.StructField, 3)
+	// fields[0].Name = "X"
+	// fields[0].Type = "int"
+	//
+	// fields[1].Name = "Y"
+	// fields[1].Type = "int"
+	//
+	// fields[2].Name = "Z"
+	// fields[2].Type = "int"
+	//
+	// struct_string, err := gnrtr.GenStruct("Point", fields)
+	// if err != nil {
+	// 	fmt.Printf("%s\n", err)
+	// } else {
+	// 	fmt.Printf("%s\n", struct_string)
+	// }
+	//
+	// func_t := gnrtr.FuncType{
+	// 	Name:   "add",
+	// 	Args:   "(int x, int y)",
+	// 	Return: "int",
+	// }
+	//
+	// func_type, err := gnrtr.GenFuncType(func_t)
+	// if err != nil {
+	// 	fmt.Printf("%s\n", err)
+	// } else {
+	// 	fmt.Printf("%s\n", func_type)
+	// }
 
-	fields[1].Name = "Y"
-	fields[1].Type = "int"
+	msgBoxA_func := gnrtr.FuncType{
+		Name:   "MessageBoxA",
+		Params: "(HWND hWnd, LPCSTR lpText, LPCSTR lpCaption, UINT uType)",
+		Args:   "(hWnd, lpText, lpCaption, uType)",
+		Return: "int",
+	}
 
-	fields[2].Name = "Z"
-	fields[2].Type = "int"
-
-	struct_string, err := gnrtr.GenStruct("Point", fields)
+	// Generate hooks
+	hook, err := gnrtr.GenHook(msgBoxA_func)
 	if err != nil {
 		fmt.Printf("%s\n", err)
 	} else {
-		fmt.Printf("%s\n", struct_string)
+		fmt.Printf("%s\n", hook)
+	}
+
+	hooks := make([]gnrtr.FuncType, 1)
+	hooks[0] = msgBoxA_func
+
+	// Generate hook table
+	hook_table, err := gnrtr.GenHookTable(hooks)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+	} else {
+		fmt.Printf("%s\n", hook_table)
 	}
 }
